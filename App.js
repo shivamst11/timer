ea0ce9330269f94241e4b6c818998f015a2a986d
shivamst11 <shivamtripathist11@gmail.com>
2021-10-07 19:04:41 +0530

    installed Momentjs

--- App.js
+++ App.js
@@ -1,51 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
+  AppState,
   StyleSheet,
   Text,
   View,
-  Dimensions,
+  StatusBar,
   TouchableOpacity,
-} from 'react-native';
+  Dimensions,
+} from "react-native";
 
-const { height, width } = Dimensions.get('screen');
+const screen = Dimensions.get("window");
+
+const formatNumber = (number) => `0${number}`.slice(-2);
+
+const getRemaining = (time) => {
+  const mins = Math.floor(time / 60);
+  const secs = time - mins * 60;
+  return { mins: formatNumber(mins), secs: formatNumber(secs) };
+};
 
 export default function App() {
-  const RoundButton = ({ title, color, onPress }) => {
-    return (
-      <TouchableOpacity
-        style={{
-          borderColor: color,
-          height: width / 2,
-          width: width / 2,
-          borderRadius: width / 2,
-          borderWidth: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginBottom: 20,
-        }}
-      >
-        <Text style={{ fontSize: 45, color: color, fontWeight: 'bold' }}>
-          {title}
-        </Text>
-      </TouchableOpacity>
-    );
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
+
+  const toggle = () => {
+    setIsActive(!isActive);
   };
+
+  const reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs((remainingSecs) => remainingSecs + 1);
+      }, 1000);
+    } else if (!isActive && remainingSecs !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs]);
 
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 90,
-          color: 'white',
-          marginTop: 175,
-          marginBottom: 20,
-        }}
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+      <TouchableOpacity onPress={toggle} style={styles.button}>
+        <Text style={styles.buttonText}>{isActive ? "Pause" : "Start"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={reset}
+        style={[styles.button, styles.buttonReset]}
       >
-        {'00'}:{'00'}
-      </Text>
-      <RoundButton title={'Start'} color={'violet'} />
-      <RoundButton title={'Reset'} color={'orange'} />
+        <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -53,7 +65,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
+    backgroundColor: "#07121B",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderWidth: 10,
+    borderColor: "#B9AAFF",
+    width: screen.width / 2,
+    height: screen.width / 2,
+    borderRadius: screen.width / 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 45,
+    color: "#B9AAFF",
+  },
+  timerText: {
+    color: "#fff",
+    fontSize: 90,
+    marginBottom: 20,
+  },
+  buttonReset: {
+    marginTop: 20,
+    borderColor: "#FF851B",
+  },
+  buttonTextReset: {
+    color: "#FF851B",
   },
 });
